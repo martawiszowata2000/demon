6aa3a03c7a360d07b43f5e85cf3e6feb57fd7eda
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-09 20:36:33 +0200

    Update timestamp.c

--- sources/timestamp.c
+++ sources/timestamp.c
@@ -2,20 +2,20 @@
 #include <sys/stat.h>
 #include <utime.h>
 
-void klonuj_timestamp(char *plikZrodlowy, char *plikDocelowy){
+void kopiuj_timestamp(char* sciezka_zrodlowy, char* sciezka_docelowy);
     struct stat st;
     struct utimbuf new_times;
-    stat(plikZrodlowy, &st);
+    stat(sciezka_zrodlowy, &st);
     new_times.actime = st.st_atim.tv_sec;
     new_times.modtime = st.st_mtim.tv_sec;
-    utime(plikDocelowy, &new_times);
-    chmod(plikDocelowy, st.st_mode);
+    utime(sciezka_docelowy, &new_times);
+    chmod(sciezka_docelowy, st.st_mode);
 }
 
-bool porownaj_timestamp(char *plikZrodlowy, char *plikDocelowy){
+bool porownaj_timestamp(char* sciezka_zrodlowy, char* sciezka_docelowy){
     struct stat st1;
     struct stat st2;
-    stat(plikZrodlowy, &st1);
-    stat(plikDocelowy, &st2);
+    stat(sciezka_zrodlowy, &st1);
+    stat(sciezka_docelowy, &st2);
     return (st1.st_mtim.tv_sec == st2.st_mtim.tv_sec);
 }
