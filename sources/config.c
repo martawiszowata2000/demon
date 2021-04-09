7cacdd0897a2e42e11774198fc9cfa3eac51d29f
mprzyst <59484241+mprzyst@users.noreply.github.com>
2021-04-09 20:16:48 +0200

    Create config.c

--- sources/config.c
+++ sources/config.c
@@ -1,1 +1,6 @@
+#include "config.h"
 
+config default_config(){
+    config DEFAULT_CONFIG = { false, 300, false, 4194304, NULL, NULL }; // invalid, 300s = 5min, non recursive, 4194304 = 2^22 = 4MB, brak katalogu, brak katalogu
+    return DEFAULT_CONFIG;
+}
