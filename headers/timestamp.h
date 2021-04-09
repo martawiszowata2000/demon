311f487d00fcc44972e82719b88a50af2596eb30
mprzyst <59484241+mprzyst@users.noreply.github.com>
2021-04-09 20:15:32 +0200

    Create timestamp.h

---
+++ headers/timestamp.h
@@ -0,0 +1,11 @@
+#ifndef TIMESTAMP_H
+#define TIMESTAMP_H
+
+#include "bool.h"
+
+void kopiuj_timestamp(char* sciezka_zrodlowy, char* sciezka_docelowy);
+
+bool porownaj_timestamp(char* sciezka_zrodlowy, char* sciezka_docelowy);
+
+
+#endif
