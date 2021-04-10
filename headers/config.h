ae0b9bf99012b493260c75fe5674668f2026e9b6
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-10 12:10:38 +0200

    Update config.h

--- headers/config.h
+++ headers/config.h
@@ -7,12 +7,12 @@
 //struktura przechowywujaca biezaca konfiguracje programu
 
 typedef struct {
-  int czasSpania; // czas spania procesu podany w sekundach
-  bool rekursywnaSynch; // czy synchronizacja ma byc rekursywna
-  bool czyPrawidlowa; // czy dana konfiguracja jest prawidlowa
-  char *sciezkaZrodlowa; // sciezka do folderu zrodlowego
-  char *sciezkaDocelowa; // sciezka do folderu docelowego
-  size_t prog; // prog dzielacy pliki na male i duze - w bajtach
+    bool czyPrawidlowa; // czy dana konfiguracja jest prawidlowa
+    int czasSpania; // czas spania procesu podany w sekundach
+    bool rekursywnaSynch; // czy synchronizacja ma byc rekursywna
+    size_t prog; // prog dzielacy pliki na male i duze - w bajtach
+    char *sciezkaZrodlowa; // sciezka do folderu zrodlowego
+    char *sciezkaDocelowa; // sciezka do folderu docelowego
 } config;
 config domyslna_konfiguracja(); // funkcja zwracajaca domyslna konfiguracje
 
