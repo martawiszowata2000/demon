df5ac56e6cf3ab6779fedb62b0a8cb9739cb0797
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-24 17:23:25 +0200

    Update config.c

--- sources/config.c
+++ sources/config.c
@@ -1,6 +1,6 @@
 #include "config.h"
 
 config domyslna_konfiguracja(){
-    config DEFAULT_CONFIG = { false, 300, false, 4194304, NULL, NULL }; // invalid, 300s = 5min, non recursive, 4194304 = 2^22 = 4MB, brak katalogu, brak katalogu
+    config DEFAULT_CONFIG = { false, 300, false, 4194304, NULL, NULL }; // nieprawidłowy, 300s = 5min, nierekursywny, 4194304 = 2^22 = 4MB, brak katalogu, brak katalogu
     return DEFAULT_CONFIG;
 }
