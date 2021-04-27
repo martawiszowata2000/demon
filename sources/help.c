b2c53dbb26f4f198e114bd1797377e35a73031e8
mprzyst <59484241+mprzyst@users.noreply.github.com>
2021-04-27 16:49:30 +0200

    Update help.c

--- sources/help.c
+++ sources/help.c
@@ -3,21 +3,20 @@
 
 void help(bool full){
     printf("Poprawne uzycie programu:\n\n");
-    printf(" (1) demon <sciezka zrodlowa> <sciezka docelowa> [opcje]\n");
-    printf(" (2) demon <opcja>\n\n");
-
-    printf("Jezeli chcesz uruchomic demona i rozpoczac synchronizacje, uzyj rozwiazania (1).\n");
-    printf("Jezeli chcesz wymusic synchronizacje lub zatrzymaa uruchomionego demona, uzyj rozwiazania (2).\n");
+    printf("1. Aby uruchomic demona i zaczac synchronizacje:\n");
+    printf(" demon <sciezka zrodlowa> <sciezka docelowa> [opcje]\n\n");
+    printf("2. Aby wymusic synchronizacje lub zatrzymac uruchomionego demona:\n");
+    printf("demon <opcja>\n\n");
 
     if(!full){
-        printf("\nWyswietlanie pomocy: demon -h\n");
+        printf("Aby wyswietlic pomoc: demon -h\n");
         return;
     }
 
-    printf("\nOpis opcji stosowanych w (1) przypadku uzycia:\n\n");
+    printf("\nOpcje stosowane w 1. przypadku:\n\n");
     help_params();
 
-    printf("\nOpis opcji stosowanych w (2) przypadku uzycia:\n\n");
+    printf("\nOpcje stosowane w 2. przypadku:\n\n");
     help_actions();
 }
 
@@ -25,12 +24,12 @@ void help_params(){
     printf(" -R : kopiowanie rekursywne\n\n");
 
     printf(" -s <liczba>[jednostka] : czas uspienia demona, jednostki:\n");
-    printf("  [ brak ] : to samo co s\n");
+    printf("  [ brak ] : patrz: s\n");
     printf("  s : sekundy\n");
     printf("  m : minuty\n");
     printf("  h : godziny\n\n");
 
-    printf(" -m <liczba>[jednostka] : pliki tego rozmiaru i wieksze beda kopiowane za pomocą mmap, jednostki:\n");
+    printf(" -m <liczba>[jednostka] : pliki tego rozmiaru i wieksze kopiowane sa za pomoca mmap, jednostki:\n");
     printf("  [ brak ] : bajty\n");
     printf("  k : kilobajty \n");
     printf("  m : megabajty\n");
@@ -38,7 +37,7 @@ void help_params(){
 }
 
 void help_actions(){
-    printf(" -F : powoduje natychmiastowe wybudzenie demona (synchronizacje)\n");
+    printf(" -F : wybudzenie demona (synchronizacja)\n");
     printf(" -S : bezpieczne zakonczenie pracy demona (konczy synchronizacje, jesli rozpoczeta)\n");
-    printf(" -K : natychmiastowe zakonczenie pracy demona.\n");
+    printf(" -K : zakonczenie pracy demona.\n");
 }
