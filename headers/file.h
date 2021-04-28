02cde6a2d97920ee6d29e8d8c6a09206036c7065
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-28 18:46:16 +0200

    Update file.h

--- headers/file.h
+++ headers/file.h
@@ -6,46 +6,42 @@
 #include "filetype.h"
 #include "config.h"
 
-/* Sprawdza jakiego typu jest plik podany w parametrze:
- * symlink, katalog, zwykły plik itp. */
+// Sprawdza jakiego typu jest plik
 FILE_TYPE pobierz_typ_pliku(const char* sciezka);
 
-/* Sprawdza czy podany katalog rzeczywiście jest katalogiem,
- * czy istnieje oraz czy użytkownik ma prawo otworzyć ten katalog.
- * w razie błędu wyświetla komunikat na stdout. */
+// Sprawdza czy podany katalog jest katalogiem, czy istnieje i czy użytkownik ma dostęp do niego
+// jeśli nie to wyświetla komunikat 
 bool katalog_uprawnienia_error(const char *nazwa);
 
-/* Sprawdza czy jeden z katalogów zawiera drugi */
+// Sprawdza czy jeden katalog jest zawarty w drugim 
 bool czy_zawiera(const char *katalog1, const char *katalog2);
 
-/* Sprawdza czy podany katalog rzeczywiście jest katalogiem,
- * czy istnieje oraz czy użytkownik ma prawo otworzyć ten katalog.
+// Dziala tak samo jak katalog_uprawnienia_error, ale nie wyświetla komunikatu
  * Nie wyświetla żadnych komunikatów. */
 bool katalog_uprawnienia(const char *nazwa);
 
-/* Sprawdza czy plik istnieje */
+// Sprawdza czy plik istnieje
 bool czy_istnieje(const char *nazwa);
 
-/* Zwraca zawartość katalogu podanego jako parametr
- * oraz jego wszystkich podkatalogów. */
+// Zwraca zawartość katalogu przekazanego w parametrach oraz jego podkatalogów
 listaPlikow* czytaj_zawartosc_katalogu(char *sciezka, bool rekursywne);
 
-/* Tworzy pusty plik */
+// Tworzy pusty plik 
 void utworz_pusty_plik(char *sciezka);
 
-/* Kopiuje zawartość jednego pliku i tworzy identyczną kopię */
+// Kopiuje zawartosc pliku (bufor)
 void kopiuj_plik(char* sciezka_zrodlowy, char* sciezka_docelowy);
 
-/*Kopiuje plik za pomocą mmap */
+// Kopiuje zawartosc pliku (mmap)
 void kopiuj_duzy_plik(char* sciezka_zrodlowy, char* sciezka_docelowy);
 
-/* Wybór odpowiedniego kopiowania */
+// Wybiera funkcje kopiujaca kompatybilna do rozmiaru pliku
 void kopiuj_opcje(char *sciezka_zrodlowy, char *sciezka_docelowy, bool czy_duzy_plik);
 
-/* Zapisuje cały bufor do file descriptora */
+// Zapisuje cały bufor do pliku - uzywana w kopiuj_plik
 ssize_t zapisz_bufor(int fd, const void* bufor, size_t count);
 
-/*Usuwa pliki i foldery na podstawie ich obecności w katalogu źródłowym */
+// Usuwa pliki i foldery zależnie od tego czyistnieja w katalogu źródłowym 
 void usun_pliki(config c);
 
 
