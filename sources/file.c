5346ffe8f6e2decb8c5ddc0679a2ca3445b0a861
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-28 18:59:06 +0200

    Update file.c

--- sources/file.c
+++ sources/file.c
@@ -73,8 +73,8 @@ listaPlikow* czytaj_zawartosc_katalogu(char *sciezka, bool rekursywne){
 }
 
 void utworz_pusty_plik(char* sciezka){
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; // uprawnienia do pliku
-    int fd = open(sciezka, O_WRONLY | O_EXCL | O_CREAT, mode); // tworzenie pliku
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; // uprawnienia do pliku (664)
+    int fd = open(sciezka, O_WRONLY | O_EXCL | O_CREAT, mode); // tworzy pliku
     if (fd == -1) {
         syslog(LOG_CRIT, "blad - nie mozna stworzyc pliku");
         exit(EXIT_FAILURE);
@@ -88,44 +88,38 @@ void kopiuj_plik(char* sciezka_zrodlowy, char* sciezka_docelowy){
     size_t bytes_read;
     int i;
 
-    /* Otwieranie pliku źródłowego */
     int zrodlo_fd = open (sciezka_zrodlowy, O_RDONLY);
-
-    /* Tworzenie pliku docelowego */
     utworz_pusty_plik (sciezka_docelowy);
     int docel_fd = open (sciezka_docelowy, O_WRONLY);
-    /* Czytanie częściami do momentu gdy część
-     * jest mniejsza niż powinna */
+
     do {
         bytes_read = read(zrodlo_fd, bufor, sizeof(bufor));
         zapisz_bufor(docel_fd, bufor, bytes_read);
 
-        /* Pilnowanie pozycji w pliku */
+        // Pilnuje pozycje w pliku
         offset += bytes_read;
     } while(bytes_read == sizeof(bufor));
-    /* Zamykanie file descriptor */
+    
     close(zrodlo_fd);
     close(docel_fd);
 }
 
 void kopiuj_duzy_plik(char *sciezka_zrodlowy, char *sciezka_docelowy){
-    /* Deklaracje zmiennych */
     int zrodlo_fd, docel_fd;
     char *zrodlo, *docelowe;
     struct stat s;
     size_t rozmiar;
 
-    /* Plik źródłowy */
+    // zrodlowy - mapowanie
     zrodlo_fd = open(sciezka_zrodlowy, O_RDONLY);
     rozmiar = lseek(zrodlo_fd, 0, SEEK_END);
     zrodlo = mmap(NULL, rozmiar, PROT_READ, MAP_PRIVATE, zrodlo_fd, 0);
 
-    /* Plik docelowy */
+    // docelowy - mapowanie
     docel_fd = open(sciezka_docelowy, O_RDWR | O_CREAT, 0666);
     ftruncate(docel_fd, rozmiar);
     docelowe = mmap(NULL, rozmiar, PROT_READ | PROT_WRITE, MAP_SHARED, docel_fd, 0);
 
-    /* Kopiowanie */
     memcpy(docelowe, zrodlo, rozmiar);
     munmap(zrodlo, rozmiar);
     munmap(docelowe, rozmiar);
@@ -135,8 +129,10 @@ void kopiuj_duzy_plik(char *sciezka_zrodlowy, char *sciezka_docelowy){
 }
 
 void kopiuj_opcje(char *sciezka_zrodlowy, char *sciezka_docelowy, bool czy_duzy_plik){
-    if(czy_duzy_plik) kopiuj_duzy_plik(sciezka_zrodlowy, sciezka_docelowy);
-    else kopiuj_plik(sciezka_zrodlowy, sciezka_docelowy);
+    if(czy_duzy_plik) 
+        kopiuj_duzy_plik(sciezka_zrodlowy, sciezka_docelowy);
+    else 
+        kopiuj_plik(sciezka_zrodlowy, sciezka_docelowy);
 }
 
 ssize_t zapisz_bufor(int fd, const void* bufor, size_t count) {
@@ -145,41 +141,36 @@ ssize_t zapisz_bufor(int fd, const void* bufor, size_t count) {
         size_t zapisane = write(fd, bufor, count);
         if (zapisane == -1) return -1;
         else
-            /* Pilnowanie ile jescze zostało do zapisu  */
+            // sprawdza ile jest jescze do zapisania
             zostalo -= zapisane;
     }
-    /* Pilnowanie żeby nie zapisać za dużo   */
+    // pilnuje aby nie zapisac zbyt duzo 
     assert (zostalo == 0);
-    /* Liczba zapisanych bajtów to count  */
     return count;
 }
 
 void usun_pliki(config c){
-    /* Tworzenie i odwrócenie listy z plikami
-     * katalogu docelowego oraz tworzenie punktu
-     * powrotu (begin) */
-    listaPlikow *lista = czytaj_zawartosc_katalogu(c.sciezkaDocelowa, c.rekursywnaSynch);
-    listaPlikow *lista_odwrocona = lista_odwroc(lista);
-    listaPlikow *poczatek = lista_odwrocona;
+    listaPlikow *lista = czytaj_zawartosc_katalogu(c.sciezkaDocelowa, c.rekursywnaSynch); // tworzy liste z plikami docelowego katalogu
+    listaPlikow *lista_odwrocona = lista_odwroc(lista); // odwraca wyzej utworzona liste
+    listaPlikow *poczatek = lista_odwrocona; // poczatek list
     lista_czysc(lista);
 
     while(lista_odwrocona->next != NULL){
         lista_odwrocona = lista_odwrocona->next;
 
-        /* Tworzenie pełnej ścieżki pliku z katalogu źródłowego */
+        // tworzy sciezke pliku z katalogu zrodlowego
         int dlugosc = strlen(lista_odwrocona->sciezka) + strlen(lista_odwrocona->nazwa) - strlen(c.sciezkaDocelowa) + strlen(c.sciezkaZrodlowa) + 2;
         char plik_zrodlowy[dlugosc];
         snprintf(plik_zrodlowy, dlugosc, "%s%s/%s", c.sciezkaZrodlowa, lista_odwrocona->sciezka + strlen(c.sciezkaDocelowa), lista_odwrocona->nazwa);
 
-        /* Tworzenie pełnej ścieżki pliku z katalogu docelowego */
+        // tworzy z docelowego
         dlugosc = strlen(lista_odwrocona->sciezka) + strlen(lista_odwrocona->nazwa) + 3;
         char do_usuniecia[dlugosc];
         snprintf(do_usuniecia, dlugosc, "%s/%s", lista_odwrocona->sciezka, lista_odwrocona->nazwa);
 
-        /* Jeżeli plik nie istnieje w katalogu źródłowym
-         * jest on usuwany z katalogu docelowego */
+        // usuwa plik z katalogu docelowego jesli plik nie istnieje w źródłowym
         if(!czy_istnieje(plik_zrodlowy)){
-            syslog(LOG_INFO, "Usuwam: %s", do_usuniecia);
+            syslog(LOG_INFO, "usuwam: %s", do_usuniecia);
             if(lista_odwrocona->typ == DIRECTORY){
                 rmdir(do_usuniecia);
             }
@@ -188,7 +179,6 @@ void usun_pliki(config c){
             }
         }
     }
-    /* Usuwanie listy */
     lista_czysc(poczatek);
 }
 
